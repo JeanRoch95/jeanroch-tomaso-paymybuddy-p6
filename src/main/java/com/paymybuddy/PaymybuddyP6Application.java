0b61d780eb059dcae089cfb30e50da0541109c07
Jean-Roch <82611021+JeanRoch95@users.noreply.github.com>
2023-07-11 08:14:23 +0200

    Create projet

---
+++ src/main/java/com/paymybuddy/PaymybuddyP6Application.java
@@ -0,0 +1,13 @@
+package com.paymybuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymybuddyP6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymybuddyP6Application.class, args);
+    }
+
+}
